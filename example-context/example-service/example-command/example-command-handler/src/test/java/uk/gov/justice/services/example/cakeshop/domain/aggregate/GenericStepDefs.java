ade1ce187d82860b82db0b01e7300dd3eca0e621
khanhassan <lionkhan@gmail.com>
2017-03-30 14:27:50 +0100

    jackson mapper is used to compare two objects

--- example-context/example-service/example-command/example-command-handler/src/test/java/uk/gov/justice/services/example/cakeshop/domain/aggregate/GenericStepDefs.java
+++ example-context/example-service/example-command/example-command-handler/src/test/java/uk/gov/justice/services/example/cakeshop/domain/aggregate/GenericStepDefs.java
@@ -2,13 +2,19 @@ package uk.gov.justice.services.example.cakeshop.domain.aggregate;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.apache.commons.lang3.StringUtils;
+import uk.gov.justice.domain.annotation.Event;
+import uk.gov.justice.services.messaging.JsonObjectEnvelopeConverter;
 
 import java.io.IOException;
 import java.lang.reflect.Constructor;
-import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -20,34 +26,33 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import static java.util.stream.Collectors.toList;
-import static org.hamcrest.CoreMatchers.instanceOf;
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class GenericStepDefs {
     private Stream<Object> events;
     private Class<?> clazz;
     private Object object;
-    private static final String fmt = "%24s: %s%n";
+    private JsonObjectEnvelopeConverter converter = new JsonObjectEnvelopeConverter();
 
     @Given("no previous events")
     public void no_previous_events() throws ClassNotFoundException, IllegalAccessException, InstantiationException {
         assert events == null;
     }
 
-    @When("the method (.*) is called on the aggregate (.*)")
-    public void add_new_receipe_2(final String methodName, final String aggregate, final String message) throws Exception {
-        checkIfAggregateCreated(aggregate);
+    @When("(.*) is called on the (.*)")
+    public void call_method_with_params(final String methodName, final String aggregate, final String message) throws Exception {
+        createAggregate(aggregate);
         Class<?>[] pType = paramsTypes(methodName);
         ObjectMapper mapper = new ObjectMapper();
         JsonNode actualNode = mapper.readTree(message);
         Map argumentsMap = mapper.convertValue(actualNode, Map.class);
         List valuesList = new ArrayList(argumentsMap.values());
         checkIfUUID(valuesList);
-        Object[] methodArgs = methodArgs(valuesList);
+        Object[] methodArgs = methodArgs(valuesList, getAllEventNames(argumentsMap));
 
         Method method = object.getClass().getMethod(methodName, pType);
         if (argumentsMap.size() == 0) {
@@ -57,12 +62,25 @@ public class GenericStepDefs {
         }
     }
 
-    private Object[] methodArgs(List valuesList) throws Exception {
+    private List<String> getAllEventNames(Map argumentsMap) {
+        List<String> classNames = new ArrayList();
+        Iterator it = argumentsMap.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry pair = (Map.Entry) it.next();
+            if (pair.getValue() instanceof HashMap) {
+                classNames.add((String) pair.getKey());
+            }
+        }
+        return classNames;
+    }
+
+    private Object[] methodArgs(List valuesList, List<String> expectedEventNames) throws Exception {
         Object[] objects = new Object[valuesList.size()];
+        int objectIndexInJson = 0;
         for (int index = 0; index < valuesList.size(); index++) {
             if (valuesList.get(index) instanceof HashMap) {
-                //TODO get the class name dynamically from json
-                objects[index] = instantiate(new ArrayList(((HashMap) valuesList.get(index)).values()), "uk.gov.moj.cpp.structure.domain.Suspect");
+                objects[index] = instantiate(new ArrayList(((HashMap) valuesList.get(index)).values()), expectedEventNames.get(objectIndexInJson));
+                objectIndexInJson = objectIndexInJson + 1;
             } else {
                 objects[index] = valuesList.get(index);
             }
@@ -70,9 +88,10 @@ public class GenericStepDefs {
         return objects;
     }
 
+
     static Object instantiate(List<String> args, String className) throws Exception {
         // Load the class.
-        Class<?> clazz = Class.forName(className);
+        Class<?> clazz = classWithFullyQualifiedClassName(className);
 
         // Search for an "appropriate" constructor.
         for (Constructor<?> ctor : clazz.getConstructors()) {
@@ -93,6 +112,24 @@ public class GenericStepDefs {
         }
 
         throw new IllegalArgumentException("Don't know how to instantiate " + className);
+    }
+
+    private static Class<?> classWithFullyQualifiedClassName(String className) {
+        Class<?> clazz = null;
+        final Package[] packages = Package.getPackages();
+
+        for (final Package p : packages) {
+            final String pack = p.getName();
+            final String tentative = pack + "." + StringUtils.capitalize(className);
+            try {
+                clazz = Class.forName(tentative);
+
+            } catch (final ClassNotFoundException e) {
+                continue;
+            }
+            break;
+        }
+        return clazz;
     }
 
     static Object convert(Class<?> target, Object s) {
@@ -139,34 +176,46 @@ public class GenericStepDefs {
     }
 
     @Then("the events are generated with following data")
-    public void new_recipe_event_generated(final String message) throws ClassNotFoundException, IOException, IllegalAccessException, InstantiationException {
-        ObjectMapper mapper = new ObjectMapper();
-        JsonNode actualNode = mapper.readTree(message);
-        List<String> expectedEvents = mapper.convertValue(actualNode.get("events"), List.class);
-        final List<Object> eventList = events.collect(toList());
-        assertThat(eventList.size(), is(expectedEvents.size()));
-        Iterator it = mapper.convertValue(actualNode, Map.class).entrySet().iterator();
-        for (int index = 0; index < expectedEvents.size(); index++) {
-            final Object event = eventList.get(index);
-            final String expectedEvent = expectedEvents.get(index);
-            assertThat(event, instanceOf(Class.forName(expectedEvent)));
-            for (Field field : event.getClass().getDeclaredFields()) {
-                field.setAccessible(true);
-                while (it.hasNext()) {
-                    Map.Entry pair = (Map.Entry) it.next();
-                    if (field.getName().equalsIgnoreCase((String) pair.getKey())) {
-                        assertThat(field.get(event).toString(), is(pair.getValue().toString()));
-                        break;
-                    }
-                    it.remove(); // avoids a ConcurrentModificationException
-                }
+    public void new_recipe_event_generated(final String message) throws ClassNotFoundException,
+            IOException, IllegalAccessException, InstantiationException {
+
+        ObjectMapper mapper = mapper();
+        final ArrayNode fromJson = (ArrayNode) mapper.readTree(message);
+        final List fromEvents = events.collect(Collectors.toList());
+
+        assertEquals(fromJson.size(), fromEvents.size());
+
+        for (int index = 0; index < fromEvents.size(); index++) {
+            assertTrue(fromJson.get(index).path("eventName").asText().equalsIgnoreCase(eventName(fromEvents.get(index))));
+            removeEventNameElement(fromJson, index);
+            Iterator i1 = fromJson.get(index).elements();
+            Iterator i2 = mapper.valueToTree(fromEvents.get(index)).elements();
+            while (i1.hasNext() && i2.hasNext()) {
+                assertTrue(i1.next().equals(i2.next()));
             }
         }
     }
 
-    private void checkIfAggregateCreated(final String aggregate) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+    private void removeEventNameElement(ArrayNode ls1, int index) {
+        ObjectNode object = (ObjectNode) ls1.get(index);
+        object.remove("eventName");
+    }
+
+    private String eventName(Object obj) throws ClassNotFoundException {
+        Class<?> cls = Class.forName(obj.getClass().getName());
+        return cls.getAnnotation(Event.class).value();
+    }
+
+    private ObjectMapper mapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        return mapper;
+    }
+
+    private void createAggregate(final String aggregate) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
         if (clazz == null && object == null) {
-            this.clazz = Class.forName(aggregate);
+            this.clazz = classWithFullyQualifiedClassName(aggregate);
             this.object = clazz.newInstance();
         }
     }
--- example-context/example-service/example-command/example-command-handler/src/test/resources/receipe.feature
+++ example-context/example-service/example-command/example-command-handler/src/test/resources/receipe.feature
@@ -1,9 +1,13 @@
+# 1) The Event Classes should have Declared Fields and Constructor params in same order
+# 2) Json should contain the Events and following json in same order
+# 3) Event json object should be in order according to constructor parameters
+
 Feature: Recipe Management
 
   Scenario: Add a recipe in system
 
     Given no previous events
-    When the method addRecipe is called on the aggregate uk.gov.justice.services.example.cakeshop.domain.aggregate.Recipe
+    When addRecipe is called on the Recipe
  """
   {
   "recipeId": "5c5a1d30-0414-11e7-93ae-92361f002671",
@@ -23,30 +27,28 @@ Feature: Recipe Management
   """
     Then the events are generated with following data
   """
-  {
-  "recipeId": "5c5a1d30-0414-11e7-93ae-92361f002671",
-  "name": "cheese cake",
-  "glutenFree": true,
-  "ingredients": [{
-  "name": "custard",
-  "quantity": 2
-  }, {
-  "name": "egg",
-  "quantity": 6
-  }, {
-  "name": "sugar",
-  "quantity": 500
-  }],
-  "events": [
-  "uk.gov.justice.services.example.cakeshop.domain.event.RecipeAdded"
-  ]
-  }
-  """
+[{
+	"eventName": "example.recipe-added",
+	"recipeId": "5c5a1d30-0414-11e7-93ae-92361f002671",
+	"name": "cheese cake",
+	"glutenFree": true,
+	"ingredients": [{
+		"name": "custard",
+		"quantity": 2
+	}, {
+		"name": "egg",
+		"quantity": 6
+	}, {
+		"name": "sugar",
+		"quantity": 500
+	}]
+}]
+"""
 
   Scenario: Rename a recipe in system
 
     Given no previous events
-    When the method addRecipe is called on the aggregate uk.gov.justice.services.example.cakeshop.domain.aggregate.Recipe
+    When addRecipe is called on the Recipe
  """
   {
   "recipeId": "5c5a1d30-0414-11e7-93ae-92361f002671",
@@ -66,26 +68,24 @@ Feature: Recipe Management
   """
     Then the events are generated with following data
   """
-  {
-  "recipeId": "5c5a1d30-0414-11e7-93ae-92361f002671",
-  "name": "cheese cake",
-  "glutenFree": true,
-  "ingredients": [{
-  "name": "custard",
-  "quantity": 2
-  }, {
-  "name": "egg",
-  "quantity": 6
-  }, {
-  "name": "sugar",
-  "quantity": 500
-  }],
-  "events": [
-  "uk.gov.justice.services.example.cakeshop.domain.event.RecipeAdded"
-  ]
-  }
+[{
+	"eventName": "example.recipe-added",
+	"recipeId": "5c5a1d30-0414-11e7-93ae-92361f002671",
+	"name": "cheese cake",
+	"glutenFree": true,
+	"ingredients": [{
+		"name": "custard",
+		"quantity": 2
+	}, {
+		"name": "egg",
+		"quantity": 6
+	}, {
+		"name": "sugar",
+		"quantity": 500
+	}]
+}]
   """
-    When the method renameRecipe is called on the aggregate uk.gov.justice.services.example.cakeshop.domain.aggregate.Recipe
+    When renameRecipe is called on the Recipe
   """
   {
   "name": "apple pie"
@@ -93,19 +93,17 @@ Feature: Recipe Management
   """
     Then the events are generated with following data
   """
-  {
-  "name": "cheese cake",
-  "events": [
-  "uk.gov.justice.services.example.cakeshop.domain.event.RecipeRenamed"
-  ]
-  }
+  [{
+  	"eventName": "example.recipe-renamed",
+  	"recipeId": "5c5a1d30-0414-11e7-93ae-92361f002671",
+  	"name": "apple pie"
+  }]
   """
-
 
   Scenario: Remove a recipe in system
 
     Given no previous events
-    When the method addRecipe is called on the aggregate uk.gov.justice.services.example.cakeshop.domain.aggregate.Recipe
+    When addRecipe is called on the Recipe
  """
   {
   "recipeId": "5c5a1d30-0414-11e7-93ae-92361f002671",
@@ -125,44 +123,40 @@ Feature: Recipe Management
   """
     Then the events are generated with following data
   """
-  {
-  "recipeId": "5c5a1d30-0414-11e7-93ae-92361f002671",
-  "name": "cheese cake",
-  "glutenFree": true,
-  "ingredients": [{
-  "name": "custard",
-  "quantity": 2
-  }, {
-  "name": "egg",
-  "quantity": 6
-  }, {
-  "name": "sugar",
-  "quantity": 500
-  }],
-  "events": [
-  "uk.gov.justice.services.example.cakeshop.domain.event.RecipeAdded"
-  ]
-  }
+[{
+	"eventName": "example.recipe-added",
+	"recipeId": "5c5a1d30-0414-11e7-93ae-92361f002671",
+	"name": "cheese cake",
+	"glutenFree": true,
+	"ingredients": [{
+		"name": "custard",
+		"quantity": 2
+	}, {
+		"name": "egg",
+		"quantity": 6
+	}, {
+		"name": "sugar",
+		"quantity": 500
+	}]
+}]
   """
-
-    When the method removeRecipe is called on the aggregate uk.gov.justice.services.example.cakeshop.domain.aggregate.Recipe
+    When removeRecipe is called on the Recipe
   """
   {
   }
   """
     Then the events are generated with following data
   """
-  {
-  "events": [
-  "uk.gov.justice.services.example.cakeshop.domain.event.RecipeRemoved"
-  ]
-  }
+ [{
+  	"eventName": "example.recipe-removed",
+  	"recipeId": "5c5a1d30-0414-11e7-93ae-92361f002671"
+  }]
   """
 
   Scenario: Make Cake
 
     Given no previous events
-    When the method addRecipe is called on the aggregate uk.gov.justice.services.example.cakeshop.domain.aggregate.Recipe
+    When addRecipe is called on the Recipe
  """
   {
   "recipeId": "5c5a1d30-0414-11e7-93ae-92361f002671",
@@ -181,39 +175,36 @@ Feature: Recipe Management
   }
   """
     Then the events are generated with following data
-  """
-  {
-  "recipeId": "5c5a1d30-0414-11e7-93ae-92361f002671",
-  "name": "cheese cake",
-  "glutenFree": true,
-  "ingredients": [{
-  "name": "custard",
-  "quantity": 2
-  }, {
-  "name": "egg",
-  "quantity": 6
-  }, {
-  "name": "sugar",
-  "quantity": 500
-  }],
-  "events": [
-  "uk.gov.justice.services.example.cakeshop.domain.event.RecipeAdded"
-  ]
-  }
-  """
 
-    When the method makeCake is called on the aggregate uk.gov.justice.services.example.cakeshop.domain.aggregate.Recipe
+  """
+[{
+	"eventName": "example.recipe-added",
+	"recipeId": "5c5a1d30-0414-11e7-93ae-92361f002671",
+	"name": "cheese cake",
+	"glutenFree": true,
+	"ingredients": [{
+		"name": "custard",
+		"quantity": 2
+	}, {
+		"name": "egg",
+		"quantity": 6
+	}, {
+		"name": "sugar",
+		"quantity": 500
+	}]
+}]
+"""
+    When makeCake is called on the Recipe
   """
   {
   "cakeId": "005d6a20-7efe-4697-afa3-2cbb282ec82f"
   }
   """
     Then the events are generated with following data
+
   """
-  {
-  "cakeId": "005d6a20-7efe-4697-afa3-2cbb282ec82f",
-  "events": [
-  "uk.gov.justice.services.example.cakeshop.domain.event.CakeMade"
-  ]
-  }
+[{
+  	"eventName": "example.cake-made",
+  	"cakeId": "005d6a20-7efe-4697-afa3-2cbb282ec82f"
+  }]
   """
